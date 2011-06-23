cb37f27f5da19a31bb939606bfba5438345ad090
Thomas von Deyen <tvdeyen@gmail.com>
2011-06-23 18:25:14 +0200

    - removed Deface deprication notices - removed some buttons and changed skin

--- lib/spree_editor_hooks.rb
+++ lib/spree_editor_hooks.rb
@@ -1,18 +1,32 @@
 class SpreeEditorHooks < Spree::ThemeSupport::HookListener
   # Admin configuration
-  insert_after :admin_configurations_menu do
-    %(<tr>
-      <td><%= link_to t("rich_editor"), admin_editor_settings_path %></td>
-      <td><%= t("rich_editor_description") %></td>
-      </ tr>)
-  end
-
-  insert_after :admin_configurations_sidebar_menu do
-    %(<li<%== ' class="active"' if controller.controller_name == 'editor_settings' %>><%= link_to t("rich_editor"), admin_editor_settings_path %></li>)
-  end
-
-  # Javascript
-  insert_after :admin_inside_head do
-    %(<%= render :partial => 'shared/rich_editor_javascript' %>)
-  end
+  Deface::Override.new(
+    :virtual_path => "admin/configurations/index",
+    :insert_after => "[data-hook='admin_configurations_menu']",
+    :text => %(
+      <tr>
+        <td><%= link_to t('rich_editor', :default => 'Richtext Editor'), admin_editor_settings_path %></td>
+        <td><%= t('rich_editor_description', :default => 'Richtext Editor Settings') %></td>
+      </tr>
+    )
+  )
+  
+  Deface::Override.new(
+    :virtual_path => "admin/shared/_configuration_menu",
+    :insert_bottom => "[data-hook='admin_configurations_sidebar_menu']",
+    :text => %(
+      <li<%== ' class="active"' if controller.controller_name == 'editor_settings' %>>
+        <%= link_to t("rich_editor", :default => 'Richtext Editor'), admin_editor_settings_path %>
+      </li>
+    )
+  )
+  
+  Deface::Override.new(
+    :virtual_path => "layouts/admin",
+    :name => "richtext_editor_javascripts",
+    :insert_bottom => "[data-hook='admin_inside_head'], #admin_inside_head[data-hook]",
+    :partial => "shared/rich_editor_javascript",
+    :disabled => false
+  )
+  
 end
